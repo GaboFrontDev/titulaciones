13bdbc3b3ab916f1cb9ae84e242134e8f79cd6e1
Gabriel Omar Hinojoza Velarde <omar.hinojoza13@gmail.com>
2019-11-27 00:26:05 -0600

    arquitectura base del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "/",
+    loadChildren: () =>
+      import("./modules/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "/login",
+    loadChildren: () =>
+      import("./modules/login/login.module").then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
