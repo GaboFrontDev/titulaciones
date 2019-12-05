e763b5576874dd14a8a01e4ebd3b0041197661d8
Hinojoza <gabriel.hinojoza@hp.com>
2019-12-05 16:34:34 -0600

    intento reciente de medir el texto interno del archivo

--- src/app/modules/home/components/vista-previa/vista-previa.component.ts
+++ src/app/modules/home/components/vista-previa/vista-previa.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import documento from '../../interfaces/documento.interface';
 import { Fechas } from './util_fechas';
 
@@ -15,6 +15,9 @@ export class VistaPreviaComponent implements OnInit {
   get documento(): documento {
     return this._documento;
   }
+
+  @ViewChild("texto", { static: true }) contenedorTexto: ElementRef;
+
   _cuerpo: string = "";
   _documento: documento;
   fechas = new Fechas();
@@ -39,6 +42,7 @@ export class VistaPreviaComponent implements OnInit {
     while (PROFESORES.includes(","))
       PROFESORES = PROFESORES.replace(",", "\n");
     this._cuerpo = `En la ciudad de Guadalajara, en la fecha ${dia} (${this.fechas.numero[dia]}) del mes de ${this.fechas.mes[mes]} de ${anio} se reunieron en el ${CU} los C. Profesores \n\n${PROFESORES} \nMiembros del Jurado designados por el Comité de Titulación de la Lienciatura en ${value.alumno.carrera} quienes realizaron la Ceremonia de Titulación de la Pasante: ${value.alumno.nombre} quien para obtener el Título de ${value.alumno.titulo} cubrió los requisitos del Reglamento General de Titulación de acuerdo al artículo 14, fracción III y los numerales artículo 13, fracción III, del reglamento Reglamento de titulación de este Centro se presentó bajo la modalidad de: ${value.modalidad} opción ${value.opcion} \n\nse realizó la evaluación al sustentante con el trabajo "${value.trabajo}" contestando satisfactoriamente las interrogantes formuladas por el jurado, el cual posteriormente procedió a la deliberación \n\nen la que se consideró aprobada con la calificación ${value.calificacion}. Finalmente el  PRESIDENTE DEL JURADO tomó a la sustentante la protesta de ley, a la cuál contestó  "SI PROTESTO". Con lo anterior, se dio por terminada la ceremonia, firmando el acta quienes en ella intervinieron`
+    console.log(this.contenedorTexto)
   }
 
 }
